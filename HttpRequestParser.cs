b76b7e6724f7d5f79d796ff1a71bd7c813a40458
Carlos Gutierrez <165713841+Baron-Von-Bonmeallow@users.noreply.github.com>
2025-08-12 12:56:05 -0700

    Update HttpRequestParser.cs

--- HttpRequestParser.cs
+++ HttpRequestParser.cs
@@ -4,7 +4,11 @@ namespace HttpRequestParser
   {
   public HttpRequest ParserRequest(string requestText)
     {
-    
+    if (requestText == null) throw new ArgumentNullException(nameof(requestText));
+
+    if(string.IsNullOrWhiteSpace(reuestText)) throw new ArgumentException("",nameof(requestText));
+
+    var request = new HttpRequest();
     }
   }
   
