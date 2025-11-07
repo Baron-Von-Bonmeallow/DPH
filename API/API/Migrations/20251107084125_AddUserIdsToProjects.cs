ae97817f937e3e90651ca7deebbef6dc47348c76
Carlos Gutierrez <165713841+Baron-Von-Bonmeallow@users.noreply.github.com>
2025-11-07 00:43:18 -0800

    API3

---
+++ API/API/Migrations/20251107084125_AddUserIdsToProjects.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdsToProjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerId",
+                table: "Projects",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserIds",
+                table: "Projects",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "UserIds",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerId",
+                table: "Projects",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf0927528ba9f26c40bacc5f6b3c69062d4a024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3af1cc8a3551616f43e9d1eb9a54c84c919fce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
